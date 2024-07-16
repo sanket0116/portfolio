1ca8485e274d8197b84e72bfa14281b03f33e962
sanket0116 <165482906+sanket0116@users.noreply.github.com>
2024-07-16 11:02:03 +0530

    first commit

---
+++ src/app/edu/edu.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edu',
+  standalone: true,
+  imports: [],
+  templateUrl: './edu.component.html',
+  styleUrl: './edu.component.css'
+})
+export class EduComponent {
+
+}
