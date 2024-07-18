ab9549aa2bc5cece8eebd0fefd70c2dbd6a58df8
sanket0116 <165482906+sanket0116@users.noreply.github.com>
2024-07-18 18:57:35 +0530

    Basic changes

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
@@ -15,5 +15,19 @@ export class NavbarComponent {
 
   toggleNavbar() {
     this.navbarOpen = !this.navbarOpen;
+  }
+
+  handleLinkClick() {
+    if (window.innerWidth < 992) { 
+      this.toggleNavbar();
+    }
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event) {
+    const target = event.target as Window;
+    if (target.innerWidth >= 992) {
+      this.navbarOpen = false; 
+    }
   }
 }
